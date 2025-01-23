25d63da1964daeb16be4d51f47664cec51fa47d1
Alex Abatov <a.aliksant315@gmail.com>
2025-01-23 20:52:18 +0900

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,287 @@
+"use client";
+
 import Image from "next/image";
+import profile from "../public/profile.jpg";
+import ThemeToggle from "../components/ui/theme-toggle";
+import { motion } from "framer-motion";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import { Card, CardContent } from "@/components/ui/card";
+import image1 from "../public/3.png";
+import image2 from "../public/item-1.jpg";
+import image3 from "../public/item-3.jpg";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+    <div className="min-h-screen bg-white dark:bg-black text-black dark:text-white transition-colors duration-300">
+      <div className="mx-auto max-w-xl px-4 py-20">
+        <motion.header
+          initial={{ opacity: 0, x: -100 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1 }}
+          className="flex items-center justify-between mb-12"
+        >
+          <div className="w-20 h-20 rounded-full overflow-hidden">
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+              src={profile}
+              alt="profile"
+              className="cursor-pointer transition-all duration-300 hover:scale-110"
+            ></Image>
+          </div>
+          <ThemeToggle />
+        </motion.header>
+
+        <main className="space-y-10">
+          <section className="space-y-10">
+            <motion.div
+              initial={{ opacity: 0, x: 100 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 1 }}
+              className="space-y-1"
+            >
+              <h1 className="text-4xl font-bold bg-gradient-to-r from-rose-600 via-indigo-500 to-sky-500 bg-clip-text text-transparent inlin-block">
+                Abatov Olloshukur
+              </h1>
+              <p className="text-gray-600 dark:text-gray-400">
+                Software Engineer & Frontend Expert
+              </p>
+              <p className="text-gray-600 dark:text-gray-400">
+                Seoul, South Korea
+              </p>
+            </motion.div>
+
+            <motion.div
+              initial={{ opacity: 0, x: -100 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 1 }}
+              className="space-y-3"
+            >
+              <h2 className="text-lg font-semibold">
+                Building Scalable Solutions for Future
+              </h2>
+              <p className="text-gray-600 text-sm dark:text-gray-400 max-w-2x1">
+                I am an expert in web and software development, specializing in{" "}
+                <span className="text-black dark:text-white">Javascript</span>{" "}
+                and <span className="text-black dark:text-white">React</span>.My
+                expertise includes{" "}
+                <span className="text-black dark:text-white">
+                  TypeScript, React, Next, Redux, NodeJs, Python, PostgresQl,
+                  MongoDB and Docker
+                </span>
+                .
+              </p>
+
+              <p className="text-gray-600 text-sm dark:text-gray-400 max-w-2x1">
+                In addition to web applications, I am passionate about AI agents
+                and developing scalable LLMs that deliver exceptional
+                performance and resilience.
+              </p>
+            </motion.div>
+
+            <motion.div
+              initial={{ opacity: 0, x: 50 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 1 }}
+              className="flex items-center gap-3"
+            >
+              <Button className="rounded-full bg-gradient-to-r from-rose-600 to-indigo-600 text-white transition-transform hover:scale-105">
+                Resume
+              </Button>
+
+              <Link
+                href="https://github.com/alikssant"
+                className="text-gray-600 dark:text-gray-400 hover:text-black dark:hover:text-white transition-colors"
+              >
+                <FaGithub className="w-6 h-6"></FaGithub>
+              </Link>
+
+              <Link
+                href="https://www.linkedin.com/in/abatov-80b6b2303/"
+                className="text-gray-600 dark:text-gray-400 hover:text-black dark:hover:text-white transition-colors"
+              >
+                <FaLinkedin className="w-6 h-6"></FaLinkedin>
+              </Link>
+
+              <Link
+                href="mailto:aliksant315@gmail.com"
+                className="text-gray-600 dark:text-gray-400 hover:text-black dark:hover:text-white transition-colors"
+              >
+                <MdEmail className="w-7 h-7"></MdEmail>
+              </Link>
+            </motion.div>
+          </section>
+
+          <motion.section
+            initial={{ opacity: 0, y: 100 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1 }}
+            className="space-y-8"
           >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+            <h2 className="text-3xl font-semibold bg-gradient-to-r from-rose-600 via-indigo-500 to-sky-500 bg-clip-text text-transparent inline-block">
+              Experience
+            </h2>
+            <div className="space-y-2">
+              <div className="flex items-center justify-between">
+                <div className="space-y-1">
+                  <p className="text-sm text-gray-600 dark:text-gray-400">
+                    May 2024 - Jan 2025
+                  </p>
+                  <p className="font-medium">Software Engineer (remote)</p>
+                  <p className="text-blue-600 dark:text-blue-400">
+                    IT PARK Uzbekistan
+                  </p>
+                </div>
+              </div>
+
+              <p className="text-gray-600 dark:text-gray-400 text-sm">
+                Led the development of cloud-native applications using AWS
+                services, implementing servless architecture that reduced
+                operational consts by 40%. Estiblishing CI/CD pipelines and
+                automated deployment processes, resulting in 60% faster releases
+                cycles.
+              </p>
+            </div>
+            <div className="space-y-2">
+              <div className="flex items-center justify-between">
+                <div className="space-y-1">
+                  <p className="text-sm text-gray-600 dark:text-gray-400">
+                    Sep 2023 - Dec 2023
+                  </p>
+                  <p className="font-medium">Internship (remote, Istanbul)</p>
+                  <p className="text-blue-600 dark:text-blue-400">
+                    Union Group Lttd
+                  </p>
+                </div>
+              </div>
+
+              <p className="text-sm text-gray-600 dark:text-gray-400">
+                Improved overall functionality and user experience. Created
+                landing pages and updated the styles of various pages. Mentored
+                by a Senior Engineer. Implemented internationalization for
+                Turkish and English languages.
+              </p>
+            </div>
+          </motion.section>
+
+          <motion.section
+            initial={{ opacity: 0, y: 100 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1 }}
+            className="space-y-8"
+          >
+            <h2 className="text-3xl font-semibold bg-gradient-to-r from-rose-600 via-indigo-500 to-sky-500 bg-clip-text text-transparent inline-block">
+              Education
+            </h2>
+
+            <div className="space-y-2">
+              <div className="flex items-center justify-between">
+                <div className="space-y-1">
+                  <p className="text-sm text-gray-600 dark:text-gray-400">
+                    Sep 2020 - Aug 2024
+                  </p>
+                  <p className="font-medium">
+                    Bachelor of Engineering - BE, Computer Science
+                  </p>
+                  <p className="text-blue-600 dark:text-blue-400">
+                    Sejong University
+                  </p>
+                </div>
+              </div>
+              <p className="text-gray-600 dark:text-gray-400 tex-sm">
+                Courses: Algorithms, Computer Science, Data
+                Structures, Software Development, Artificial Intelligence (AI),
+                C/C++, C#, Web Development, and Computer
+                Networks.
+              </p>
+            </div>
+          </motion.section>
+
+          <motion.section
+            initial={{ opacity: 0, y: 100 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1 }}
+            className="space-y-8"
+          >
+            <h2 className="text-3xl font-semibold bg-gradient-to-r from-rose-600 via-indigo-500 to-sky-500 bg-clip-text text-transparent inline-block">
+              Projects
+            </h2>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-3">
+              <Card className="bg-gray-50 dark:bg-zinc-900 border-gray-200 dark:border-zinc-800 transition-transform duration-300 hover:scale-105 cursor-pointer">
+                <CardContent className="p-4">
+                  <Image
+                    src={image1}
+                    alt="image1"
+                    className="rounded-lg mb-4"
+                  />
+
+                  <div className="flex items-center justify-between">
+                    <div>
+                      <h3 className="font-medium">Codefit</h3>
+                      <p className="text-sm text-gray-600 dark:text-gray-400">
+                        Next.js, MySQl, Node.js, React.js
+                      </p>
+                    </div>
+                    <Link href="https://github.com/alikssant/">
+                      <Button variant="ghost" size="icon">
+                        🚀
+                      </Button>
+                    </Link>
+                  </div>
+                </CardContent>
+              </Card>
+              <Card className="bg-gray-50 dark:bg-zinc-900 border-gray-200 dark:border-zinc-800 transition-transform duration-300 hover:scale-105 cursor-pointer">
+                <CardContent className="p-4">
+                  <Image
+                    src={image2}
+                    alt="image1"
+                    className="rounded-lg mb-4"
+                  />
+
+                  <div className="flex items-center justify-between">
+                    <div>
+                      <h3 className="font-medium">WriteIt</h3>
+                      <p className="text-sm text-gray-600 dark:text-gray-400">
+                        React, Redux, Firebase
+                      </p>
+                    </div>
+                    <Link href="https://github.com/alikssant/WriteIt">
+                      <Button variant="ghost" size="icon">
+                        🚀
+                      </Button>
+                    </Link>
+                  </div>
+                </CardContent>
+              </Card>
+              <Card className="bg-gray-50 dark:bg-zinc-900 border-gray-200 dark:border-zinc-800 transition-transform duration-300 hover:scale-105 cursor-pointer">
+                <CardContent className="p-4">
+                  <Image
+                    src={image3}
+                    alt="image1"
+                    className="rounded-lg mb-4"
+                  />
+
+                  <div className="flex items-center justify-between">
+                    <div>
+                      <h3 className="font-medium">tvScout</h3>
+                      <p className="text-sm text-gray-600 dark:text-gray-400">
+                        React.js, API, Javascript
+                      </p>
+                    </div>
+                    <Link href="https://github.com/alikssant/tvScout">
+                      <Button variant="ghost" size="icon">
+                        🚀
+                      </Button>
+                    </Link>
+                  </div>
+                </CardContent>
+              </Card>
+            </div>
+            <p className="text-gray-500">@ 2025 Abatov Olloshukur</p>
+          </motion.section>
+        </main>
+      </div>
     </div>
   );
 }
